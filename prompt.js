b3719b19bb2da4f46f935c963594137ccff65e3c
Thompson <thompc6@byui.edu>
2019-05-21 15:05:02 -0600

    commeted out some code

--- prompt.js
+++ prompt.js
@@ -14,39 +14,39 @@ var d3 = require('d3-dsv')
 var arrayFormat = {arrayFormat: 'bracket'}
 
 inquirer.prompt(questions).then(async answers => {
-    console.log(JSON.stringify(answers, null, '  '));
+/*     console.log(JSON.stringify(answers, null, '  '));
     console.log(
         "\n"+
         "       __|__\n"+
         "--@--@--(_)--@--@--\n") 
-
+ */
     var newAnswers = [];
     newAnswers.push(answers);
-    console.log(newAnswers)
+ /*    console.log(newAnswers)
     console.log(
         "\n"+
         "       __|__\n"+
-        "--@--@--(_)--@--@--\n")  
+        "--@--@--(_)--@--@--\n")   */
 
     delete answers.filters
     delete answers.Output
     //
-    console.log(answers)
+/*     console.log(answers)
     console.log(
         "\n"+
         "       __|__\n"+
-        "--@--@--(_)--@--@--\n")  
+        "--@--@--(_)--@--@--\n")   */
 
     
     const stringified = query_string.stringify(answers, {arrayFormat: 'bracket'});
-    console.log(stringified) 
+/*     console.log(stringified) 
     console.log(
         "\n"+
         "       __|__\n"+
         "--@--@--(_)--@--@--\n") 
 
     const parse = query_string.parseUrl('https://byui.instructure.com/api/v1/accounts/1/courses?'+stringified);
-    console.log(parse)
+    console.log(parse) */
 
 
 
@@ -55,16 +55,18 @@ inquirer.prompt(questions).then(async answers => {
     const results = await canvas.get('/api/v1/accounts/1/courses?'+stringified)
     //console.log(results)
 
-    var courses = JSON.stringify(results)
+    results.forEach(course =>{
 
+    })
+    console.log(results.length)
+    var courses = JSON.stringify(results + '\n')
 
-
-        fs.appendFile("courses.json", courses, function(err) {
+        fs.writeFile("courses.json", courses, function(err) {
             if (err) console.log(err);
-                console.log('complete');
+                console.log('complete :D');
             }
-          );
+          ); 
 
 
 
-})
+}).catch(console.log)
